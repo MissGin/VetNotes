1d74881d89f2cc417db64ac5d6a99999e82a4eea
MissGin <bj.brewer@att.net>
2023-02-26 04:16:39 -0900

    Add files via upload

---
+++ whiskas.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://mandtbsiwbmixeyvqnhz.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbmR0YnNpd2JtaXhleXZxbmh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYxMTI0MDMsImV4cCI6MTk5MTY4ODQwM30.BkJt-iwZT7qPM-B2hKT7G7GN5OjSR06hVDlQWr-eea8'
+        
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
